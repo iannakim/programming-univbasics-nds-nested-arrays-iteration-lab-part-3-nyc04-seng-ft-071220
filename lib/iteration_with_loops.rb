5b4752270b5d3f26a3825d59527e676eb6744524
Hyun Ji  Kim <dear.hyunji@gmail.com>
2020-07-21 12:55:58 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ while row_of_index < src[row_of_index].length do
   inner_array=0
   while inner_array < src[row_of_index][inner_array].length do
     if src[row_of_index][inner_array].class == String
-       phrase= src[row_of_index][inner_array].join(
+       phrase= src[row_of_index][inner_array].join
        result_string<<phrase
        inner_array=+1
   end
