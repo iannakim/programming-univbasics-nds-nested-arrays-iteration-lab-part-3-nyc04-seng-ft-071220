b9e6d2216aae65228bf67d3628ead954619b6b74
Hyun Ji  Kim <dear.hyunji@gmail.com>
2020-07-21 13:03:01 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,10 +1,8 @@
 def join_nested_strings(src)
-  result_string=[]
   row_of_index=0
 while row_of_index < src[row_of_index].length do
       phrase = src[row_of_index].join
-       result_string<<phrase
   row_of_index+=1
 end
-  p result_string
+  p phrase
 end
